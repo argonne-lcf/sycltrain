6c491e97a570b128dcc5d6ac20de0a3ce5352ea6
TApplencourt <t.applencourt@anl.gov>
2020-06-17 14:55:19 +0000

    Update Makefile

--- 9_sycl_of_hell/7_buffer_usm.cpp
+++ 9_sycl_of_hell/7_buffer_usm.cpp
@@ -1,10 +1,34 @@
+#include "cxxopts.hpp"
 #include <CL/sycl.hpp>
 
 namespace sycl = cl::sycl;
 
 int main(int argc, char **argv) {
 
-  const auto global_range = (size_t)atoi(argv[1]);
+  //  _                ___
+  // |_) _. ._ _  _     |  ._  ._     _|_
+  // |  (_| | _> (/_   _|_ | | |_) |_| |_
+  //                           |
+
+  cxxopts::Options options("7 buffer usm",
+                           " How to use Unifed shared memory");
+
+  options.add_options()("h,help", "Print help")(
+      "g,grange", "Global Range", cxxopts::value<int>()->default_value("1"));
+
+  auto result = options.parse(argc, argv);
+
+  if (result.count("help")) {
+    std::cout << options.help({"", "Group"}) << std::endl;
+    exit(0);
+  }
+
+  const auto global_range = result["grange"].as<int>();
+
+ //            _           __                                              
+ // | | ._  o _|_ _   _|   (_  |_   _. ._ _   _|   ._ _   _  ._ _   _  ._   
+ // |_| | | |  | (/_ (_|   __) | | (_| | (/_ (_|   | | | (/_ | | | (_) | \/ 
+ //                                                                      / 
 
   // Selectors determine which device kernels will be dispatched to.
   sycl::default_selector selector;
@@ -13,8 +37,7 @@ int main(int argc, char **argv) {
   sycl::device dev = myQueue.get_device();
   sycl::context ctex = myQueue.get_context();
 
-  int *A = static_cast<int *>(
-      sycl::malloc_shared(global_range * sizeof(int), dev, ctex));
+  int *A = static_cast<int *>(sycl::malloc_shared(global_range * sizeof(int), dev, ctex));
 
   // Advise runtime how memory will be used
   auto e = myQueue.mem_advise(A, global_range * sizeof(int), PI_MEM_ADVICE_SET_NON_ATOMIC_MOSTLY);
