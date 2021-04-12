a01d49af2d0863dad13403aa99951a60f3dd50f2
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2021-04-12 12:54:39 -0500

    Update 7_allocator_usm.cpp

--- 9_sycl_of_hell/7_allocator_usm.cpp
+++ 9_sycl_of_hell/7_allocator_usm.cpp
@@ -31,8 +31,7 @@ int main(int argc, char **argv) {
   sycl::queue myQueue;
 
   // Create usm allocator
-  sycl::usm_allocator<float, sycl::usm::alloc::shared> allocator(
-      myQueue.get_context(), myQueue.get_device());
+  sycl::usm_allocator<float, sycl::usm::alloc::shared> allocator(myQueue);
   // Allocate value
   std::vector<float, decltype(allocator)> A(global_range, allocator);
 
@@ -44,17 +43,12 @@ int main(int argc, char **argv) {
   auto *A_p = A.data();
 
   // Create a command_group to issue command to the group
-  myQueue.submit([&](sycl::handler &cgh) {
-    // No accessor needed!
-    cgh.parallel_for<class hello_world>(
+  myQueue.parallel_for<class hello_world>(
         sycl::range<1>{sycl::range<1>(global_range)},
         [=](sycl::id<1> idx_id) {
           const int idx = idx_id[0];
           A_p[idx] = idx;
-        }); // End of the kernel function
-  });       // End of the queue commands
-  // Wait for the kernel to complete
-  myQueue.wait();
+  }).wait();
 
   for (size_t i = 0; i < global_range; i++)
     std::cout << "A[ " << i << " ] = " << A[i] << std::endl;
