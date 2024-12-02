051b32fe848cbbd3c41c26ae8a251202fc3fb76c
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-12-02 22:09:17 +0000

    Use google style and format source files

--- 9_sycl_of_hell/2_parallel_for.cpp
+++ 9_sycl_of_hell/2_parallel_for.cpp
@@ -1,8 +1,8 @@
-#include "argparse.hpp"
 #include <sycl/sycl.hpp>
 
-int main(int argc, char **argv) {
+#include "argparse.hpp"
 
+int main(int argc, char **argv) {
   //  _                ___
   // |_) _. ._ _  _     |  ._  ._     _|_
   // |  (_| | _> (/_   _|_ | | |_) |_| |_
@@ -28,14 +28,16 @@ int main(int argc, char **argv) {
   // |  (_| | (_| | | (/_ |   | (_) |
 
   sycl::queue Q;
-  std::cout << "Running on " << Q.get_device().get_info<sycl::info::device::name>() << std::endl;
+  std::cout << "Running on "
+            << Q.get_device().get_info<sycl::info::device::name>() << std::endl;
 
   // Similar to
   //   # pragma OMP PARALLEL FOR
   //   for(int idx=0; idx++; idx < global_range)
   Q.parallel_for(global_range, [=](sycl::id<1> idx) {
     // Explicit cast because of printf shenaningan.
-    sycl::ext::oneapi::experimental::printf("Hello, World! World rank %d\n", static_cast<int>(idx));
+    sycl::ext::oneapi::experimental::printf("Hello, World! World rank %d\n",
+                                            static_cast<int>(idx));
   });
   Q.wait();
 
--- 9_sycl_of_hell/6_in_order.cpp
+++ 9_sycl_of_hell/6_in_order.cpp
@@ -1,8 +1,8 @@
-#include "argparse.hpp"
 #include <sycl/sycl.hpp>
 
-int main(int argc, char **argv) {
+#include "argparse.hpp"
 
+int main(int argc, char **argv) {
   //  _                ___
   // |_) _. ._ _  _     |  ._  ._     _|_
   // |  (_| | _> (/_   _|_ | | |_) |_| |_
@@ -30,7 +30,8 @@ int main(int argc, char **argv) {
   //
   sycl::queue Q{sycl::property::queue::in_order()};
 
-  std::cout << "Running on " << Q.get_device().get_info<sycl::info::device::name>() << "\n";
+  std::cout << "Running on "
+            << Q.get_device().get_info<sycl::info::device::name>() << "\n";
 
   // Allocate Device Memory
   int *A = sycl::malloc_device<int>(global_range, Q);
