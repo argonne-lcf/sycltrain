0b5e529d4bf0cc38cf854830a57adf7fa4283804
Thomas Applencourt <tapplencourt@anl.gov>
2025-04-29 16:59:25 -0500

    Update 8_submit_lazy_greedy.cpp

--- advanced_inferno/8_submit_lazy_greedy.cpp
+++ advanced_inferno/8_submit_lazy_greedy.cpp
@@ -4,17 +4,15 @@
 #include <thread>
 
 int main(int argc, char **argv) {
-  using namespace std::chrono_literals;
-
   sycl::queue Q;
   auto *ptr = sycl::malloc_shared<int>(1, Q);
-  *ptr = 0;
-
   auto f = [=]() {
     sycl::atomic_ref<int, sycl::memory_order_acq_rel,
                      sycl::memory_scope::system>(*ptr)
         .store(1);
   };
+  
+  *ptr = 0;
   const auto start = std::chrono::high_resolution_clock::now();
   Q.single_task(f);
   Q.wait();
@@ -22,14 +20,14 @@ int main(int argc, char **argv) {
   const std::chrono::duration<double, std::micro> elapsed = (end - start);
 
   *ptr = 0;
-  std::cout << "Submiting Kernel who will set the sentinel to 1" << std::endl;
+  std::cout << "Submitting Kernel who will set the sentinel to 1" << std::endl;
   Q.single_task(f);
   std::cout << "Sleep 4 times the estimated durations of the kernel"
             << std::endl;
   std::this_thread::sleep_for(elapsed * 4);
   std::cout << "Setting the sentinel to 2" << std::endl;
   *ptr = 2;
-  std::cout << "Now waiting for the kernel to finish" << std::endl;
+  std::cout << "Waiting for the Kernel to finish" << std::endl;
   Q.wait();
 
   std::cout << "Sentinel Value: " << *ptr << std::endl;
